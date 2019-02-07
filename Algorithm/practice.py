b24c367e12b24d0c41c9a351df4b31fa7a78b52a
JongMin (Home) <jongm711@gmail.com>
2019-02-07 23:48:29 +0900

    Update 190207

--- Algorithm/practice.py
+++ Algorithm/practice.py
@@ -3,36 +3,47 @@ sys.stdin = open("input.txt","r")
 
 c = int(input())
 
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+puzzles = [
+    [[0,0],[0,1],[1,0]],
+    [[0,0],[1,-1],[1,0]],
+    [[0,0],[0,1],[1,1]],
+    [[0,0],[1,0],[1,1]]
+]
+types = [0,1,2,3]
 direction = 0
 
-def detect (i,j,mapInfo,direction):
-    x = j+dx[direction]
-    y = i+dy[direction]
-    dot1 = mapInfo[y][x]
-    dot2 = mapInfo[y+1][x+1]
-    if dot1 =='.' and dot2 == '.' :
-        mapInfo[i][j] = '#'
-        dot1 = '#'
-        dot2 = '#'
-    return mapInfo
-
-def solution(h,w,mapInfo, cnt):
+def solution(y,x,mapInfom,typ):
     n = mapInfo.count('.')
-    if n%3 != 0 :
-        return cnt
+    nextX = 0
     if n == 0 :
-        return cnt
-    for j in range(w):
-        if mapInfo[h-1][j] != '#' and mapInfo [h-1][j] == '.':
-            detect(i,j,mapInfo,0)
+        return 1
+
+    for xy in puzzles[types[typ]] :
+        pieceX = x+xy[1]
+        pieceY = y+xy[0]
+        nextX += xy[1] if xy[1] > 0 else 0
+        if pieceX<h and pieceX>=0 and pieceY<w and pieceY>=0 and mapInfo[pieceY][pieceX]!="#":
+            mapInfo[pieceY][pieceX] = "#"
+            if x+nextX >= w :
+                x = 0
+                return solution(y+1,x,mapInfo,0)+solution(y+1,x,mapInfo,1)+solution(y+1,x,mapInfo,2)+solution(y+1,x,mapInfo,3)
+            else :
+                return solution(y,x+nextX,mapInfo,0)+solution(y,x+nextX,mapInfo,1)+solution(y,x+nextX,mapInfo,2)+solution(y,x+nextX,mapInfo,3)
             
-                       
+
 for i in range(c):
     # h는 세로 w는 가로
     h, w = map(int,input().split())
-    mapInfo = [list(input().split()) for _ in range(h)]
+    mapInfo = [[i for i in input()] for _ in range(h)]
+    x,y=-1,-1
+    for i in mapInfo :
+        for j in i :
+            if j == '.':
+                x=i.index(j)
+                y=mapInfo.index(i)
+                break
+        if x > -1 : break
     cnt = 0
-    print(f'{solution(h,w,mapInfo,cnt)}')
+    print(x,y)
+    print(f'{solution(y,x,mapInfo,cnt)}')
 
---
+++ HTML/miniproject/app.py
@@ -0,0 +1,95 @@
+from flask import Flask,render_template, request, redirect
+import sqlite3
+import datetime
+
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+@app.route('/articles/')
+def articles():
+    """DB에 저장된 모든 글들을 보여준다"""
+    # 1. DB에 접속하여
+    db = sqlite3.connect('blog.db') 
+    c = db.cursor()
+    sql = "SELECT*FROM articles"
+    c.execute(sql)
+    # 2. 저장된 모든 글들을 가져온다.(fetchall())
+    data = c.fetchall()
+    # 3. index.html에 넣어서 보여준다.
+    return render_template('articles.html',data=data)
+    
+@app.route('/articles/new/')
+def new():
+    dt = datetime.datetime.now()
+    time = dt.strftime("%Y.%d.%m %H:%M:%S")
+    return render_template('new.html',time=time)
+    
+@app.route('/articles/create/')
+def create():
+    title = request.args.get('title')
+    content = request.args.get('content')
+    author = request.args.get('author')
+    created_at = request.args.get('created_at')
+    
+    db = sqlite3.connect('blog.db') 
+    c = db.cursor()
+    sql = "INSERT INTO articles (title,content,author,created_at) VALUES ('{}','{}','{}','{}')".format(title,content,author,created_at)
+    c.execute(sql)
+    db.commit()
+    
+    sql = "SELECT article_id FROM articles WHERE title='{}' AND author='{}'".format(title,author)
+    c.execute(sql)
+    article_id=c.fetchone()
+    print(article_id)
+    return redirect('/articles/{}'.format(article_id[0]))
+    
+@app.route('/articles/<int:article_id>')
+def detail(article_id):
+    db = sqlite3.connect('blog.db') 
+    c = db.cursor()
+    sql = "SELECT * FROM articles WHERE article_id = {}".format(article_id)
+    c.execute(sql)
+    data = c.fetchone()
+    return render_template("detail.html",data=data)
+    
+@app.route('/articles/<int:article_id>/edit/')
+def edit(article_id):
+    dt = datetime.datetime.now()
+    time = dt.strftime("%Y.%d.%m %H:%M:%S")
+    
+    db = sqlite3.connect('blog.db') 
+    c = db.cursor()
+    sql = "SELECT * FROM articles WHERE article_id = {}".format(article_id)
+    c.execute(sql)
+    data = c.fetchone()
+    return render_template("edit.html",data=data,time=time)
+    
+@app.route('/articles/<int:article_id>/update')
+def update(article_id):
+    title = request.args.get('title')
+    content = request.args.get('content')
+    author = request.args.get('author')
+    created_at = request.args.get('created_at')
+    
+    db = sqlite3.connect('blog.db') 
+    c = db.cursor()
+    sql = "UPDATE articles SET title='{}',content='{}',author='{}',created_at='{}' WHERE article_id = {}".format(title,content,author,created_at,article_id)
+    c.execute(sql)
+    db.commit()
+    return redirect('/articles/{}'.format(article_id))
+    
+@app.route('/articles/<int:article_id>/delete')
+def delete(article_id):
+    db = sqlite3.connect('blog.db') 
+    c = db.cursor()
+    sql = "DELETE FROM articles WHERE article_id={}".format(article_id)
+    c.execute(sql)
+    db.commit()
+    return redirect('/articles')
+    
+    
+app.run(debug=True)
