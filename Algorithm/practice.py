724e19d8051538bbc22d274b7f1ca1e3bc15c326
JongMin (5285) <jongm711@gmail.com>
2019-02-11 21:44:22 +0900

    Update 190211

--- Algorithm/practice.py
+++ Algorithm/practice.py
@@ -1,15 +1,53 @@
 import sys
 sys.stdin = open("input.txt","r")
 
+# def merge_sort (arr):
+#    if len(arr)<=1:
+#       return arr
+#    midIdx=len(arr)//2
+#    left,right =[],[]
+#    left = arr[:midIdx]
+#    right = arr[midIdx:]
+#    left = merge_sort(left)
+#    right = merge_sort(right)
+#    return merge(left, right)
+
+# def merge (left, right):
+#    result = []
+#    while len(left) > 0 or len(right) > 0:
+#       if len(left) > 0 and len(right) > 0:
+#          if left[0] <= right[0]:
+#                result.append(left[0])
+#                left = left[1:]
+#          else:
+#                result.append(right[0])
+#                right = right[1:]
+#       elif len(left) > 0:
+#          result.append(left[0])
+#          left = left[1:]
+#       elif len(right) > 0:
+#          result.append(right[0])
+#          right = right[1:]
+#    return result
+
+# TC = int(input())
+# arr=[0 for _ in range(TC)]
+# for i in range(TC):
+#    arr[i]=int(input())
+
+# for i in merge_sort(arr):
+#    print(i)
+
 TC = int(input())
-numlist = []
-for i in range(TC):
-   n = int(input())
-   numlist.append(n)
+arr = [0 for _ in range(2000001)]
 
 for _ in range(TC):
-    for i in range(TC-1):
-        if numlist[i] > numlist[i+1] :
-            numlist[i], numlist[i+1] = numlist[i+1], numlist[i]
-for i in numlist:
-    print(i)
+   idx = int(input())
+   arr[idx+1000000] = idx
+count = 0
+for i in arr[1:] :
+   if i != 0:
+      print(i)
+      count+=1
+   if count == TC :
+      break
