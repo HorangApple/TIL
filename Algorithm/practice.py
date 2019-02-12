802324e476ee6790e330c0fd15f7207b3e9845c9
JongMin (Home) <jongm711@gmail.com>
2019-02-13 01:20:44 +0900

    Update 190213

--- Algorithm/practice.py
+++ Algorithm/practice.py
@@ -1,58 +1,35 @@
 import sys
 sys.stdin = open("input.txt","r")
 
-# def merge_sort (arr):
-#    if len(arr)<=1:
-#       return arr
-#    midIdx=len(arr)//2
-#    left,right =[],[]
-#    left = arr[:midIdx]
-#    right = arr[midIdx:]
-#    left = merge_sort(left)
-#    right = merge_sort(right)
-#    return merge(left, right)
-
-# def merge (left, right):
-#    result = []
-#    while len(left) > 0 or len(right) > 0:
-#       if len(left) > 0 and len(right) > 0:
-#          if left[0] <= right[0]:
-#                result.append(left[0])
-#                left = left[1:]
-#          else:
-#                result.append(right[0])
-#                right = right[1:]
-#       elif len(left) > 0:
-#          result.append(left[0])
-#          left = left[1:]
-#       elif len(right) > 0:
-#          result.append(right[0])
-#          right = right[1:]
+# def count_sort(arr,TC,k):
+#    countArr=[0 for _ in range(k+1)]
+#    result=[0 for _ in range(TC)]
+#    for i in range(TC) :
+#       countArr[arr[i]] +=1
+#    for i in range(1,k+1):
+#       countArr[i]+=countArr[i-1]
+#    for i in range(TC):
+#       result[countArr[arr[i]]-1]=arr[i]
+#       countArr[arr[i]]-=1
 #    return result
 
 # TC = int(input())
-# arr=[0 for _ in range(TC)]
-# for i in range(TC):
-#    arr[i]=int(input())
+# arr=[int(input()) for _ in range(TC)]
 
-# for i in merge_sort(arr):
+# for i in count_sort(arr,TC,max(arr)):
 #    print(i)
 
-TC = int(input())
-arr = [0 for _ in range(2000002)]
 
-for _ in range(TC):
-   idx = int(input())
-   if idx != 0 :
-      arr[idx+1000000] = idx
-   else :
-      arr[1000000] = 1
-count = 0
-for i in range(len(arr)) :
-   if i == 1000000 and arr[i] == 1 :
-      print(0)
-   elif arr[i] != 0 :
-      print(arr[i])
-      count+=1
-   if count == TC :
-      break
+
+
+N = int(input())
+series = [0] * 10001
+
+for i in range(N):
+    a = int(sys.stdin.readline())
+    series[a] = series[a] + 1
+
+for b in range(len(series)):
+    if series[b] !=0:
+        for c in range(series[b]):
+            print(b)
