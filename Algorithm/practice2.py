4f5c1f5bc1face46e1a80ea654dc70a2063fe6ab
JongMin (Mul) <jongm711@gmail.com>
2019-02-22 17:59:28 +0900

    Update 190222

--- Algorithm/practice2.py
+++ Algorithm/practice2.py
@@ -1,42 +1,48 @@
 import sys
 sys.stdin = open("input.txt","r")
 
-def backtrack(a,k,inp,result,check):
-    c=[0]*(inp+1)
-    if k==inp:
-        total=0
-        for i in range(inp):
-            total+=mp[i][result[i+1]-1]
-            if total>mini[0]:
-                break
+t=input()
+w=input()
+
+def fail(w):
+    output=[0]
+    for i in range(1,len(w)):
+        part=w[:i]
+        if i>0 :
+            count=0
+            for j in range(len(part)//2):
+                if part[0:j+1]==part[-1-j:]:
+                    count=len(part[0:j+1])
+            output.append(count)
+    return output
+
+def solution(t,w):
+    i=0
+    start=[]
+    count=0
+    while i<len(t):
+        j=0
+        while j<len(w):
+            if t[i+j]!=w[j]:
+                if j==0:
+                    i+=1
+                    break
+                elif move[j]==0:
+                    i+=j
+                    break
+                else :
+                    i=i+j-move[j]
+                    break
+            j+=1
         else:
-            mini[0]=total
-    else:
-        k+=1
-        construct_candidates(a,k,inp,c)
-        for i in range(1,inp+1):
-            if c[i]>0:
-                result[k]=c[i]
-                check+=mp[k-1][result[k]-1]
-                if check>mini[0]:
-                    check-=mp[k-1][c[i]-1]
-                    continue
-                a[i]=0
-                backtrack(a,k,inp,result,check)
-                a[i]=c[i]
-                check-=mp[k-1][c[i]-1]
-def construct_candidates(a,k,inp,c):
-    for i in range(1,inp+1):
-        if a[i]>0:
-            c[a[i]]=a[i]
+            start.append(i+1)
+            i+=j
+            count+=1
+    return [count,start]
 
-
-tc=int(input())
-for num in range(1,tc+1):
-    length=int(input())
-    mp=[list(map(int,input().split())) for _ in range(length)]
-    result=[0]*(length+1)
-    a=list(range(length+1))
-    mini=[999999999999999999999999999]
-    backtrack(a,0,length,result,0)
-    print(f'#{num} {mini[0]}')
+move=[]
+move=fail(w)
+result=solution(t,w)
+print(result[0])
+for i in result[1]:
+    print(i)
