2d7863bafe4fa45025ef7727cd259aad3afbc9ab
JongMin (Home) <jongm711@gmail.com>
2019-06-05 23:40:12 +0900

    redux-promise-middleware 적용

--- JavaScript/React/middleWare/src/store.js
+++ JavaScript/React/middleWare/src/store.js
@@ -2,10 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 // import loggerMiddleware from './lib/loggerMiddleware';
 import {createLogger} from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
+// import ReduxThunk from 'redux-thunk';
+
+import {createPromise } from 'redux-promise-middleware'
 
 const logger = createLogger();
+const pm = createPromise ({
+  promiseTypeSuffixes:['PENDING','SUCCESS','FAILURE']
+})
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
+const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),applyMiddleware(logger, pm))
 
 export default store;
