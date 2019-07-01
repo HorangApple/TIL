7915994f939843632323759cee1f4e9db92021b0
JongMin (Home) <jongm711@gmail.com>
2019-07-02 01:02:30 +0900

    react-blog 실습: editor 상태관리 구현 전

--- JavaScript/React/blog/blog-frontend/src/components/editor/PreviewPane/PreviewPane.js
+++ JavaScript/React/blog/blog-frontend/src/components/editor/PreviewPane/PreviewPane.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import styles from './Footer.scss';
+import styles from './PreviewPane.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
+const PreviewPane = ({markdown,title}) => (
+  <div className={cx('preview-pane')}>
+    <h1 className={cx('title')}>
+      제목
+    </h1>
+    <div>
+      내용
+    </div>
+  </div>
+);
 
-export default Footer;
+
+export default PreviewPane;
